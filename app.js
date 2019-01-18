680febf6a72aff2df67f2840e305ea3f56734a7b
vitaliyvolchenko <vitaliyvolchenkodeveloper@gmail.com>
2019-01-18 17:27:43 +0200

    Adding User Signup, Login & JWT Signing

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const userRoutes = require('./server/routes/user');
+
+mongoose.connect('mongodb://vitaliy:qa123123@ds261114.mlab.com:61114/e-learning');
+mongoose.connection.once('open', () => console.log('Connected to MongoDB'));
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
@@ -20,11 +26,13 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works!'
-    });
-});
+// app.use((req, res, next) => {
+//     res.status(200).json({
+//         message: 'It works!'
+//     });
+// });
+
+app.use("/user", userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error('Not found');
